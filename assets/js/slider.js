d701ccbc544cd825dbde965866d30e6ff047102c
shahmeerali74 <111519383+shahmeerali74@users.noreply.github.com>
2025-01-05 22:00:23 +0500

    new services added

---
+++ assets/js/slider.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1024: {
+                items: 3
+            }
+        }
+    });
+});
